cfdc03fccb2ca17c9b7ae8418a0a7840505d01f3
Manav <manavdoda7@gmail.com>
2022-02-05 04:37:49 +0530

    bug fix

--- middlewares/dbConnection.js
+++ middlewares/dbConnection.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const singleMetric = require('../models/singleMetricData')
 const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@metrics.to6ds.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 mongoose.connect(uri, (err)=>{
     if(err) console.log('Error connecting to DB', err);
     else console.log('DB connected')
